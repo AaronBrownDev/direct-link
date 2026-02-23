2f63243c4c9e00a22b169da41e4f0bab45998a49
rwiggins1 <wigginsricky94@gmail.com>
2026-02-23 22:05:31 +0000

    feat(video-core): setup EncoderConfig

---
+++ video-core/tests/unit/encode/test_encoder_config.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+#include "../../../include/encode/encoder_config.hpp"
+
+using namespace videoCore::encode;
+
+TEST(EncoderConfig, DefaultValues) {
+    EncoderConfig config;
+    EXPECT_EQ(config.width, 0);
+    EXPECT_EQ(config.height, 0);
+    EXPECT_EQ(config.framerate, 30);
+    EXPECT_EQ(config.gopSize, 60);
+    EXPECT_EQ(config.preset, EncoderConfig::Preset::UltraFast);
+    EXPECT_EQ(config.type, EncoderConfig::Type::Software);
+}
